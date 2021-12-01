f07b34f609dcd8c4a7b74c84d050d00672824483
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-12-01 09:08:34 -0500

    bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-    bot.user.setActivity(process.env.BUILD=="T"? "stable":"beta", { type: 'PLAYING' })
+    client.user.setActivity(process.env.BUILD=="T"? "stable":"beta", { type: 'PLAYING' })
     console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
 }
