332d46092e9eebefb684249d4f5d2dc1b747e002
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-12-02 14:11:27 -0500

    command fixed :)

--- commands/say.js
+++ commands/say.js
@@ -1,13 +1,10 @@
 exports.run = async(client, message, [...umessage])=>{
-    let users
-    await message.guild.members.cache.find(u=>u.id===message.author.id).then(user=>{
-        users = user
-    })
-    message.channel.createWebhook(users.displayname, {
+    let user = message.guild.members.cache.find(u=>u.id===message.author.id)
+    message.channel.createWebhook(user.displayname, {
         avatar: message.author.avatarURL(),
         reason: "User message"
     }).then(webhk=>{
-        webhk.send(`**${umessage.join(' ')}**`).then(_=>{
+        webhk.send(`${umessage.join(' ')}`).then(_=>{
             webhk.delete()
             message.delete()
         })
